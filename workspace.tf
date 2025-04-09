068b787f93cccecb12f9c916ed0b93ad0a78d491
Richard Cheney <24895350+richeney@users.noreply.github.com>
2025-04-09 15:23:58 +0100

    Commented out fabric resources

--- workspace.tf
+++ workspace.tf
@@ -1,13 +1,13 @@
-data "fabric_capacity" "fabric" {
-  display_name = "fabric85085c7c"
-}
+# data "fabric_capacity" "fabric" {
+#   display_name = "fabric85085c7c"
+# }
 
-resource "fabric_workspace" "example2" {
-  display_name = "My Terraform Workspace"
-  description  = "This is a test of the Terraform provider."
-  capacity_id  = data.fabric_capacity.fabric.id
+# resource "fabric_workspace" "example2" {
+#   display_name = "My Terraform Workspace"
+#   description  = "This is a test of the Terraform provider."
+#   capacity_id  = data.fabric_capacity.fabric.id
 
-  identity = {
-    type = "SystemAssigned"
-  }
-}
+#   identity = {
+#     type = "SystemAssigned"
+#   }
+# }
